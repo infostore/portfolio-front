7970049b8a4839a77d904fefbfeecd7cccb1cb26
jyoh <korman21@gmail.com>
2023-09-15 09:06:37 +0900

    Initial version of portfolio generated by generator-jhipster@8.0.0-beta.3

---
+++ src/main/java/com/my/pf/config/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Application configuration.
+ */
+package com.my.pf.config;
---
+++ src/main/java/com/my/pf/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package com.my.pf.repository;
+
+import com.my.pf.domain.Authority;
+import org.springframework.data.r2dbc.repository.R2dbcRepository;
+
+/**
+ * Spring Data R2DBC repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends R2dbcRepository<Authority, String> {}
